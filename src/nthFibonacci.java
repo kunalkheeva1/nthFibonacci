07907c15d6e8eb0ac22ad1f7a30f4201a051521f
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-09 23:46:19 -0700

    created a method to get nth fibonacci number with simplicity

--- src/nthFibonacci.java
+++ src/nthFibonacci.java
@@ -1,2 +1,25 @@
 public class nthFibonacci {
+
+
+
+    static long [] fibonacciCache = new long[];
+    static long mod = (long)1e9+7;
+    public static long nthFibonacci(long n){
+        if(n==0 || n== 1){
+            return n;
+        }
+
+        if(fibonacciCache[(int)n] != 0){
+            return fibonacciCache[(int)n];
+        }
+        fibonacciCache[(int)n] = (nthFibonacci(n-1) + nthFibonacci(n-2))%mod;
+
+        return fibonacciCache[(int)n];
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 }
